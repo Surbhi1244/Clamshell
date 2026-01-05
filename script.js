25dfa6d541b343241b9a6dd0c60d8dc8b3aae7eb
Surbhi1244 <helios12404@gmail.com>
2026-01-05 23:45:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+  const btn = document.getElementsByClassName("login-button")[0];
+
+  if (btn.innerText === "Continue") {
+    // Continue click hone par redirect
+    window.location.href = "main-dashboard.html";
+  } else {
+    // Pehli click par UI change
+    btn.innerText = "Continue";
+    document.getElementsByClassName("login-form-hidden")[0].style.display = "block";
+  }
+});
